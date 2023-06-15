916bfb777c17af537dc56043376b3703cebb7db8
Johannes Wilhelm <johannesf.wilhelm@gmail.com>
2023-06-15 16:10:21 +0200

    compose sequences

--- decayBuildSequences.py
+++ decayBuildSequences.py
@@ -3,6 +3,9 @@ from xmlrpc.client import MAXINT
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from tqdm import tqdm
+from matplotlib.animation import FuncAnimation
+
 
 
 df_300 = pd.read_csv("data/aligned/HistoricalData_2016_2023_300.csv")
@@ -23,19 +26,17 @@ df_21600["timescale"] = 21600
 
 
 
-
+# composes dfs of individual sequences from different timescales (in reverse)
 # dfs should be ordered fine to coarse with the finest one having the labels
 # seqLen should be divisible by the number of time-resolutions len(dfs)
-def buildTimeDecaySequences(SEQLEN, dfs, timescales, CANDLES_SHIFT):
+def composeTimeDecaySequences(SEQLEN, dfs, timescales, CANDLES_SHIFT):
     numTimeScales = len(dfs)
     seqPartLen = int(SEQLEN/numTimeScales)
 
     seqDfs = []
-    
     finestEndIndex = len(dfs[0])-1
-    while True:
+    for _ in tqdm(range(int((len(dfs[0]) -int(seqPartLen*(sum(timescales))/300))/CANDLES_SHIFT))): # calculating how many sequences there will be
         
-        print("finestEndIndex:", finestEndIndex)
         lastTimestamp = dfs[0]["timestamp"].iloc[finestEndIndex]+1
         seqDf = pd.DataFrame()
         for i in range(numTimeScales):
@@ -48,30 +49,51 @@ def buildTimeDecaySequences(SEQLEN, dfs, timescales, CANDLES_SHIFT):
         seqDfs.append(seqDf)
         finestEndIndex -= CANDLES_SHIFT
 
-    return seqDfs   
+    return seqDfs[::-1]
 
 
 
 
-    #res = []
+
+
+seqDfs = composeTimeDecaySequences(80, [df_300, df_900, df_3600, df_21600], [300, 900, 3600, 21600], 10)
+
+#plt.figure("first")
+#plt.plot(seqDfs[0]["timestamp"], seqDfs[0]["BTC_close"])
 #
-    #while len(df) >= SEQ_LEN:
-    #    first = df.head(SEQ_LEN).copy()
-    #    first.index = np.arange(0, len(first))
-    #    res.append(first)
-    #    df = df.tail(len(df) - CANDLES_SHIFT)
-    #    df.index = np.arange(0, len(df))
+#plt.figure("last")
+#plt.plot(seqDfs[-1]["timestamp"], seqDfs[-1]["BTC_close"])
 #
-    #print("-done")
-    #print("")
-    #return res
+#plt.show()
 
 
 
-#print(df_900)
-#seqDf = buildTimeDecaySequences(80, [df_21600, df_3600, df_900, df_300], [21600, 3600, 900, 300])
-seqDf = buildTimeDecaySequences(80, [df_300, df_900, df_3600, df_21600], [300, 900, 3600, 21600], 5000)
-print(seqDf)
 
-plt.plot(seqDf["timestamp"], seqDf["BTC_close"])
+
+
+
+
+
+## animate through all the sequrnces
+
+fig, ax = plt.subplots()
+
+# Initial plot
+line, = ax.plot(seqDfs[0]["timestamp"], seqDfs[0]["BTC_close"])
+ax.set_title('BTC_close vs timestamp')
+ax.set_xlabel('timestamp')
+ax.set_ylabel('BTC_close')
+
+# Update function for the animation
+def update(num):
+    ax.clear()  # Clear previous plot
+    line, = ax.plot(seqDfs[num]["timestamp"], seqDfs[num]["BTC_close"])
+    ax.set_title('BTC_close vs timestamp')
+    ax.set_xlabel('timestamp')
+    ax.set_ylabel('BTC_close')
+    return line,
+
+# Create the animation, looping back at the end
+ani = FuncAnimation(fig, update, frames=range(len(seqDfs)), interval=1, blit=True, repeat=True)
+
 plt.show()
