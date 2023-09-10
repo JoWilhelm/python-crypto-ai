c058f026c7f48609c7351170900c15ff3d7ecb5a
Johannes Wilhelm <johannesf.wilhelm@gmail.com>
2023-09-10 20:53:33 +0200

    getMarketData done

--- exchangeInterface/marketData.py
+++ exchangeInterface/marketData.py
@@ -7,6 +7,24 @@ from datetime import datetime
 client = RestClient()
 
 
+candleIntvSeconds = {
+    'MINUTE_1': 60,
+    'MINUTE_5': 60*5,
+    'MINUTE_10': 60*10,
+    'MINUTE_15': 60*15,
+    'MINUTE_30': 60*30,
+    'HOUR_1': 60*60,
+    'HOUR_2': 60*60*2,
+    'HOUR_4': 60*60*4,
+    'HOUR_6': 60*60*6,
+    'HOUR_12': 60*60*12,
+    'DAY_1': 60*60*24,
+    'DAY_3': 60*60*24*3,
+    'WEEK_1': 60*60*24*7
+}
+
+
+
 
 
 def getHistoricalDataSingleRequest(pairing, start, end, candleIntv):
@@ -22,11 +40,11 @@ def getHistoricalDataSingleRequest(pairing, start, end, candleIntv):
         columns = ['low', 'high', 'open', 'close', 'amount_quoteUnits', 'quantity_baseUnits', 'buyTakerAmount_quoteUnits', 'buyTakerQuantity_baseUnits', 'tradeCount', 'ts_recordPushed', 'weightedAverage', 'ts_startTime', 'ts_closeTime', 'dt_close']
     """
     # poloniex API request
-    response = client.markets().get_candles(pairing, 'MINUTE_5', start=start, end=end, limit=500)
+    response = client.markets().get_candles(pairing, candleIntv, start=start, end=end, limit=min(500, int((end-start)/candleIntvSeconds[candleIntv])))
     
     df = pd.DataFrame(response, columns=['low', 'high', 'open', 'close', 'amount_quoteUnits', 'quantity_baseUnits', 'buyTakerAmount_quoteUnits', 'buyTakerQuantity_baseUnits', 'tradeCount', 'ts_recordPushed', 'weightedAverage', 'interval', 'ts_openTime', 'ts_closeTime'])
     df.drop('interval', axis=1, inplace=True)
-    # convert timesteps from mili seocnds to seconds 
+    # convert timestamps from milliseconds to seconds 
     df['ts_openTime'] = df['ts_openTime'] / 1000
     df['ts_closeTime'] = df['ts_closeTime'] / 1000
     df['ts_recordPushed'] = df['ts_recordPushed'] / 1000
@@ -38,7 +56,68 @@ def getHistoricalDataSingleRequest(pairing, start, end, candleIntv):
     return df
 
 
-df = getHistoricalDataSingleRequest('BTC_USDT', start=1694264564-(60*5*500), end=1694264564, candleIntv='MINUTE_5')
+
+
+def getHistoricalData(pairing, start, end, candleIntv):
+    """Breaks one long interval up into multiple API request in order to fetch more than 500 candles of historical market data.
+
+    Args:
+        pairing (String): e.g. 'BTC_USDT'
+        start (int): timestamp start of interval
+        end (int): timestamp end of interval
+        candleIntv (String): e.g. 'MINUTE_5'
+    Returns:
+        pd.DataFrame 
+        columns = ['low', 'high', 'open', 'close', 'amount_quoteUnits', 'quantity_baseUnits', 'buyTakerAmount_quoteUnits', 'buyTakerQuantity_baseUnits', 'tradeCount', 'ts_recordPushed', 'weightedAverage', 'ts_startTime', 'ts_closeTime', 'dt_close']
+    """
+    
+    intervalLength = 500*candleIntvSeconds[candleIntv]
+    intervalStart = start
+    intervalEnd = start + intervalLength
+    intervalsCounter = 1
+    numIntervals = int((end - start) / intervalLength)
+
+    df = pd.DataFrame()
+
+    # for when API breaks access
+    ## load DF
+    #intervalsCounter = 150
+    #dataset = pd.read_csv(f"HistoricalData_2016_2023_{CANDLES_PERIOD}_{intervalsCounter}.csv")
+    #intervalStart = START + (intervalLength * intervalsCounter)
+    #intervalEnd = intervalStart + intervalLength
+    #intervalsCounter += 1
+
+    while(intervalEnd < end):
+        df = pd.concat([df, getHistoricalDataSingleRequest(pairing, intervalStart, intervalEnd, candleIntv)], ignore_index=True)
+        # shift interval
+        intervalStart = intervalEnd
+        intervalEnd += intervalLength
+
+        # counter
+        print("intervals: ", intervalsCounter, "/", numIntervals, " len dataset: ", len(df))
+
+        #if intervalsCounter % 50 == 0:
+        #    dataset = dataset.apply(pd.to_numeric)
+        #    # add additional columns
+        #    dataset["BTC_HLPercent"] = (dataset["BTC_high"] - dataset["BTC_low"]) / dataset["BTC_high"]
+        #    # print, to csv
+        #    dataset.to_csv(f"HistoricalData_2016_2023_{CANDLES_PERIOD}_{intervalsCounter}.csv", index=False)
+        #    time.sleep(60)
+        intervalsCounter += 1
+
+    intervalEnd = end
+    df = pd.concat([df, getHistoricalDataSingleRequest(pairing, intervalStart, intervalEnd, candleIntv)], ignore_index=True)
+
+    # add additional columns
+    #dataset["BTC_HLPercent"] = (dataset["BTC_high"] - dataset["BTC_low"]) / dataset["BTC_high"]
+    # print, to csv
+    #print(dataset)
+    #dataset.to_csv(f"HistoricalData_2016_2023_{CANDLES_PERIOD}.csv", index=False)
+
+    return df
+
+
+df = getHistoricalData('BTC_USDT', start=1694264564-(60*5*1600), end=1694264564, candleIntv='MINUTE_5')
 print(df)
 
 
@@ -46,54 +125,48 @@ print(df)
 
 
 
-#def getHistoricalData(pairing, start, end, candleIntv):
-#
-#
-#
-#
-#    return
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #
 #
 #
