dd6541b2a34dea05f74f9eb90a964cf264794b8c
Johannes Wilhelm <johannesf.wilhelm@gmail.com>
2023-09-19 09:51:13 +0200

    cont

--- ppComp.py
+++ ppComp.py
@@ -1,5 +1,6 @@
 #import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 import pandas as pd
 from sklearn import preprocessing
@@ -49,27 +50,64 @@ def splitDf(df):
 
 
 df = pd.read_csv("historicalData/labeled/HistoricalDataLabeled_BTC_USDT_01072016_01072023_MINUTE_5_ov40_th04p.csv")
-df = df[['low', 'high', 'open', 'close', 'quantity_baseUnits', 'HLPercent', 'dt_closeTime']]
+df = df[['low', 'high', 'open', 'close', 'quantity_baseUnits', 'HLPercent']]
 
+df = df.replace(0, 0.00001)
 
-df = df.tail(5000)
+#df = df.tail(1000)
 print(df)
 
 
 
 splittedDfs = splitDf(df)
+print(splittedDfs[0])
 
-print(splittedDfs)
+ppDfs = preprocess(splittedDfs)
+print(ppDfs[0])
+
+print(len(ppDfs))
 
 
-#plt.plot(df['close'])
+
+
+#plt.plot(ppDfs[0]['close'])
 #
 #plt.show()
 
 
 
 
-
+## Initialize the plot
+#fig, ax = plt.subplots()
+#
+## Initialize a line plot. Note that the data for the line will be updated later.
+#line, = ax.plot([], [], lw=2)
+#
+## Function to initialize the plot (required by FuncAnimation)
+#def init():
+#    line.set_data([], [])
+#    return line,
+#
+## Function to update each frame of the plot
+#def update(frame):
+#    xdata = range(len(ppDfs[frame]['close']))
+#    ydata = ppDfs[frame]['close']
+#    ax.set_xlim(0, len(xdata) - 1)
+#    ax.set_ylim(min(ydata) - 0.1, max(ydata) + 0.1)  # Optionally set y-limits to adjust automatically
+#    line.set_data(xdata, ydata)
+#    return line,
+#
+## Create an animation
+#ani = FuncAnimation(
+#    fig,
+#    update,
+#    frames=range(len(ppDfs)),  # Number of frames
+#    init_func=init,
+#    blit=True,
+#    interval=10  # Show each frame for 0.5 seconds (500 milliseconds)
+#)
+#
+#plt.show()
 
 
 
